24d6dd322352ba64a75e8499546e57053d7b6808
Praveen <npkumar97714@gmail.com>
2025-03-06 12:29:46 +0530

    Addition Example

--- Test/src/main/java/org/example/AddTwoNumbers.java
+++ Test/src/main/java/org/example/AddTwoNumbers.java
@@ -3,6 +3,8 @@ package org.example;
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int sum = 12+23;
+        int add = 33+24;
         System.out.println(sum);
+        System.out.println(add);
     }
 }
